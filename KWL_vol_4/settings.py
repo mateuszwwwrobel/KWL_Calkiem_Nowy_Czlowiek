5306ec37ae92ae948c32adb6fe0a52bf0e6d94e0
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-01-29 13:32:36 +0000

    small changes

--- KWL_vol_4/settings.py
+++ KWL_vol_4/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from decouple import config, Csv
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,8 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-#Email functionality:
-
+# Email configuration:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
