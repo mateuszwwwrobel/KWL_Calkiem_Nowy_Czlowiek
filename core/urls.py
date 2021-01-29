0196036cb69f56d96b4895963e7e81c7aa8e2424
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-01-29 13:54:42 +0000

    Change to class based views

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,12 @@
-from django.contrib import admin
 from django.urls import path
-from .views import home_view, concert_view, listen_view, order_form_view, order_complete
+from .views import HomeView, ConcertView, ListenView, OrderFormView, OrderConfirmView
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', home_view, name="home"),
-    path('concerts', concert_view, name='concerts'),
-    path('listen', listen_view, name='listen'),
-    path('order', order_form_view, name='order'),
-    path('order-complete', order_complete, name='order-complete'),
+    path('', HomeView.as_view(), name="home"),
+    path('concerts', ConcertView.as_view(), name='concerts'),
+    path('listen', ListenView.as_view(), name='listen'),
+    path('order', OrderFormView.as_view(), name='order'),
+    path('order-complete', OrderConfirmView.as_view(), name='order-complete'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,64 +3,62 @@ from django.core.mail import send_mail
 from .forms import OrderForm
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
+from django.views.generic import TemplateView, FormView, View
 
 
-def home_view(request):
-    return render(request, 'index.html')
+class HomeView(TemplateView):
+    template_name = 'index.html'
 
 
-def listen_view(request):
-    return render(request, 'listen.html')
+class ListenView(TemplateView):
+    template_name = 'listen.html'
 
 
-def concert_view(request):
-    return render(request, 'concerts.html')
+class ConcertView(TemplateView):
+    template_name = 'concerts.html'
 
 
-def order_form_view(request):
-    form = OrderForm(request.POST or None)
-
-    context = {
-        'form': form
-    }
-
-    return render(request, 'order_form.html', context)
+class OrderFormView(FormView):
+    form_class = OrderForm
+    template_name = 'order_form.html'
+    success_url = 'core:order-complete'
 
 
-def order_complete(request):
-    form = OrderForm(request.POST or None)
-    if form.is_valid():
-        order_name = request.POST.get('order_name')
-        mail = [request.POST.get('order_email'), 'kwlrec@gmail.com']
+class OrderConfirmView(View):
+    def post(self, request):
+        form = OrderForm(request.POST or None)
+        if form.is_valid():
+            order_name = request.POST.get('order_name')
+            mail = [request.POST.get('order_email'), 'kwlrec@gmail.com']
 
-        combined_address = f"{request.POST.get('order_address_1')} {request.POST.get('order_address_2')}"
+            combined_address = f"{request.POST.get('order_address_1')} {request.POST.get('order_address_2')}"
 
-        mail_data = {
-            'full_name': order_name,
-            'address': combined_address,
-            'postcode': request.POST.get('post_code'),
-            'amount': request.POST.get('order_amount'),
+            mail_data = {
+                'full_name': order_name,
+                'address': combined_address,
+                'postcode': request.POST.get('post_code'),
+                'amount': request.POST.get('order_amount'),
             }
 
-        # Email sender:
-        subject = f'Całkiem Nowy Człowiek - {str(order_name).capitalize()}'
-        html_message = render_to_string('email.html', mail_data)
-        message = strip_tags(html_message)
+            # Email sender:
+            subject = f'Całkiem Nowy Człowiek - {str(order_name).capitalize()}'
+            html_message = render_to_string('email.html', mail_data)
+            message = strip_tags(html_message)
 
-        send_mail(
-            subject, 
-            message, 
-            'matthew.sparrow91@gmail.com', 
-            mail,
-            html_message=html_message,
-        )
+            send_mail(
+                subject,
+                message,
+                'matthew.sparrow91@gmail.com',
+                mail,
+                html_message=html_message,
+            )
 
-        context = {
-            'user': order_name,
-        }
+            context = {
+                'user': order_name,
+            }
 
-        form.save()
-    else:
-        raise ValueError("Form is not valid.")
+            form.save()
+        else:
+            raise ValueError("Form is not valid.")
 
-    return render(request, 'order-complete.html', context)
+        return render(request, 'order-complete.html', context)
